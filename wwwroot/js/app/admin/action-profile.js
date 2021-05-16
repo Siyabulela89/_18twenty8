772853060167a62ddf586a29ca72929c99e5c776
Letlhogonolo Setlhodi <lg.setlhodi@hotmail.com>
2021-05-16 11:28:10 +0200

    Completed Little Sister Alert Approve

--- wwwroot/js/app/admin/action-profile.js
+++ wwwroot/js/app/admin/action-profile.js
@@ -1,8 +1,7 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
     // Action Buttons
-    var btnApprove = document.querySelector(".btn-approve-profile");
+    var btnApprove = document.querySelector("#btn-approve-profile");
     var btnDecline = document.querySelector("#btn-decline-profile");
-    var btnDeclineAssign = document.querySelector("#btn-decline-profile-assign");
     var btnCorrect = document.querySelector("#btn-correct-profile");
     var btnSaveProfileStatus = document.querySelector("#btn-save-profile-status-reason");
     var taProfileStatusReason = document.querySelector("#ta-profile-Status-reason");
@@ -11,17 +10,6 @@
     // Stage Status
     var phase2 = document.querySelector("#big-sis-phase-2");
 
-    document.body.addEventListener('click', (e) => {
-        if (e.target.parentElement.classList.contains('btn-approve-profile')) {
-            btnApprove = e;
-        }
-    });
-
-    if (btnDeclineAssign) {
-        btnDeclineAssign.addEventListener("click", () => {
-            declineProfileHandler();
-        });
-    }
 
     if (btnApprove) {
         btnApprove.addEventListener("click", (e) => {
@@ -68,13 +56,7 @@
         data.action = btnApprove.getAttribute('data-action');
         data.userId = btnApprove.getAttribute('data-user-id');
         data.role = btnApprove.getAttribute('data-role');
-        var assignment = btnApprove.getAttribute('data-src');
-        if (assignment === "assignment-action") {
-            data.sisterAssignId = btnApprove.getAttribute('data-sister-assign-id'); //data-sister-assign-id
-            actionProfileSisterAssign(data)
-        } else {
-            submitData(data);
-        }
+        submitData(data);
     }
 
     function declineProfileHandler() {
@@ -83,13 +65,7 @@
         data.userId = btnDecline.getAttribute('data-user-id');
         data.role = btnDecline.getAttribute('data-role');
         data.reason = taProfileStatusReason.value;
-        var assignment = btnApprove.getAttribute('data-src');
-        if (assignment === "assignment-action") {
-            data.sisterAssignId = btnDecline.getAttribute('data-sister-assign-id');
-            actionProfileSisterAssign(data)
-        } else {
-            submitData(data);
-        }
+        submitData(data);
     }
 
     function correctProfileHandler() {
@@ -98,46 +74,7 @@
         data.userId = btnCorrect.getAttribute('data-user-id');
         data.role = btnCorrect.getAttribute('data-role');
         data.reason = taProfileStatusReason.value;
-        var assignment = btnApprove.getAttribute('data-src');
-        assignment === "assignment-action" ? actionProfileSisterAssign(data) : submitData(data);
-    }
-
-    function actionProfileSisterAssign(data = {}) {
-        const options = {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-
-        fetch(`${window.location.origin}/Administration/ActionProfileSisterAssign`, options)
-            .then((response) => {
-                return response.json();
-            })
-            .then((response) => {
-                if (data.action == "Approved") {
-                    btnApprove.innerHTML = '<i class="fas fa-thumbs-up"></i>';
-                    phase2.innerHTML = `<i class="fas fa-check"></i>`
-                    btnApprove.disabled = true;
-                    btnApprove.style.cursor = 'default';
-                }
-
-                if (data.action == "Sent for Correction") {
-                    phase2.innerHTML = `<i class="fas fa-exclamation"></i>`
-                    btnCorrect.disabled = true;
-                    btnCorrect.style.cursor = 'default';
-                }
-
-                if (data.action == "Declined") {
-                    phase2.innerHTML = `<i class="fas fa-times"></i>`
-                    btnDecline.disabled = true;
-                    btnDecline.style.cursor = 'default';
-                }
-                btnSaveProfileStatus.innerHTML = '<i class="fas fa-save"></i> Save';
-                $('#profileStatusReasonModal').modal('hide');
-                toastr.success(`${data.action} successfully`, 'Success Message')
-            });
+        submitData(data);
     }
 
     function submitData(data = {}) {
